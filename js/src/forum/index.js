112401d8cce2a52f1f166e3a2b48d91570cdad5c
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-21 13:12:43 +0530

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -7,23 +7,21 @@ import Button from 'flarum/common/components/Button';
 
 app.initializers.add('ramesh-dada-mobile-flarum-newdiscuss', () => {
     extend(IndexPage.prototype, 'viewItems', function (items) {
-            const canStartDiscussion = app.forum.attribute('canStartDiscussion') || !app.session.user;
-    items.add(
-      'newDiscussion',
-      Button.component(
-        {
-          icon: 'fas fa-edit',
-          className: 'Button Button--primary IndexPage-newDiscussion',
-          itemClassName: 'DadaDiscuss',
-          onclick: () => {
-            // If the user is not logged in, the promise rejects, and a login modal shows up.
-            // Since that's already handled, we dont need to show an error message in the console.
-            return this.newDiscussionAction().catch(() => {});
+        const canStartDiscussion = app.forum.attribute('canStartDiscussion') || !app.session.user;
+        items.add(    'newDiscussion',
+        Button.component(
+          {
+            icon: 'fas fa-edit',
+            className: 'Button Button--primary IndexPage-newDiscussion',
+            itemClassName: 'DadaDiscuss',
+            onclick: () => {
+              // If the user is not logged in, the promise rejects, and a login modal shows up.
+              // Since that's already handled, we dont need to show an error message in the console.
+              return this.newDiscussionAction().catch(() => {});
+            },
+            disabled: !canStartDiscussion,
           },
-          disabled: !canStartDiscussion,
-        },
-        app.translator.trans(canStartDiscussion ? 'core.forum.index.start_discussion_button' : 'core.forum.index.cannot_start_discussion_button')
-      )
-    );
+          app.translator.trans(canStartDiscussion ? 'core.forum.index.start_discussion_button' : 'core.forum.index.cannot_start_discussion_button')
+        ), -100);
     });
 });
