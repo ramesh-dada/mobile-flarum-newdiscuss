ef23735f8bb238aa628f0bd1f9532acfcfed8a66
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-21 14:01:30 +0530

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,4 +1,4 @@
-import {extend} from 'flarum/common/extend';
+import { extend } from 'flarum/common/extend';
 import app from 'flarum/forum/app';
 import IndexPage from 'flarum/forum/components/IndexPage';
 import Button from 'flarum/common/components/Button';
@@ -6,22 +6,35 @@ import Button from 'flarum/common/components/Button';
 /* Super Ultra Pro RameshDADA Premium 1Dot */
 
 app.initializers.add('ramesh-dada-mobile-flarum-newdiscuss', () => {
-    extend(IndexPage.prototype, 'viewItems', function (items) {
-        const canStartDiscussion = app.forum.attribute('canStartDiscussion') || !app.session.user;
-        items.add(    'newDiscussion',
-        Button.component(
-          {
-            icon: 'fas fa-edit',
-            className: 'Button Button--primary IndexPage-newDiscussion',
-            itemClassName: 'DadaDiscuss',
-            onclick: () => {
-              // If the user is not logged in, the promise rejects, and a login modal shows up.
-              // Since that's already handled, we dont need to show an error message in the console.
-              return this.newDiscussionAction().catch(() => {});
-            },
-            disabled: !canStartDiscussion,
+  extend(IndexPage.prototype, 'viewItems', function (items) {
+    const canStartDiscussion = app.forum.attribute('canStartDiscussion') || !app.session.user;
+    items.add('newDiscussion',
+      Button.component(
+        {
+          icon: 'fas fa-edit',
+          className: 'Button Button--primary IndexPage-newDiscussion',
+          itemClassName: 'DadaDiscuss',
+          onclick: () => {
+            // If the user is not logged in, the promise rejects, and a login modal shows up.
+            // Since that's already handled, we dont need to show an error message in the console.
+            return this.newDiscussionAction().catch(() => { });
           },
-          app.translator.trans(canStartDiscussion ? 'core.forum.index.start_discussion_button' : 'core.forum.index.cannot_start_discussion_button')
-        ), -100);
-    });
+          disabled: !canStartDiscussion,
+        },
+        app.translator.trans(canStartDiscussion ? 'core.forum.index.start_discussion_button' : 'core.forum.index.cannot_start_discussion_button')
+      ), -100);
+
+    const tag = this.currentTag();
+    if (tag) {
+      const color = tag.color();
+      const canStartDiscussion = tag.canStartDiscussion() || !app.session.user;
+
+      if (color) {
+        items.get('newDiscussion').attrs.style = { backgroundColor: color };
+      }
+
+      items.get('newDiscussion').attrs.disabled = !canStartDiscussion;
+      items.get('newDiscussion').children = app.translator.trans(canStartDiscussion ? 'core.forum.index.start_discussion_button' : 'core.forum.index.cannot_start_discussion_button');
+    }
+  });
 });
