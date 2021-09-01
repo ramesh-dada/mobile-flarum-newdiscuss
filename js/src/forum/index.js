74b85839bda7b6d4c1505976ae8245aa8050cb0a
RameshDada <iusewgogoi@gmail.com>
2021-09-01 16:13:59 +0530

    Old

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -2,13 +2,11 @@ import { extend } from 'flarum/common/extend';
 import app from 'flarum/forum/app';
 import IndexPage from 'flarum/forum/components/IndexPage';
 import Button from 'flarum/common/components/Button';
-import searchmodal from './searchmodal';
 
 /* Super Ultra Pro RameshDADA Premium 1Dot */
 
 app.initializers.add('ramesh-dada-mobile-flarum-newdiscuss', () => {
-  extend(IndexPage.prototype, 'sidebarItems', (items) => {
-
+  extend(IndexPage.prototype, 'viewItems', function (items) {
     const canStartDiscussion = app.forum.attribute('canStartDiscussion') || !app.session.user;
     items.add('newDiscussion',
       Button.component(
@@ -26,14 +24,17 @@ app.initializers.add('ramesh-dada-mobile-flarum-newdiscuss', () => {
         app.translator.trans(canStartDiscussion ? 'core.forum.index.start_discussion_button' : 'core.forum.index.cannot_start_discussion_button')
       ), -100);
 
-    // Add new item
-    items.add(
-      'searchbutton',
-      <Button icon="fas fa-search" className="Button Button--primary SearchDada" onclick={() => app.modal.show(searchmodal, {})}>
-        Search
-      </Button>,
-      89
-    );
+    const tag = this.currentTag();
+    if (tag) {
+      const color = tag.color();
+      const canStartDiscussion = tag.canStartDiscussion() || !app.session.user;
 
+      if (color) {
+        items.get('newDiscussion').attrs.style = { backgroundColor: color };
+      }
+
+      items.get('newDiscussion').attrs.disabled = !canStartDiscussion;
+      items.get('newDiscussion').children = app.translator.trans(canStartDiscussion ? 'core.forum.index.start_discussion_button' : 'core.forum.index.cannot_start_discussion_button');
+    }
   });
 });
