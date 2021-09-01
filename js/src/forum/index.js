670d0d0b72924014d28727151c4e0c0739736195
RameshDada <iusewgogoi@gmail.com>
2021-09-01 15:57:08 +0530

    Stable

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -2,11 +2,12 @@ import { extend } from 'flarum/common/extend';
 import app from 'flarum/forum/app';
 import IndexPage from 'flarum/forum/components/IndexPage';
 import Button from 'flarum/common/components/Button';
+import searchmodal from './searchmodal';
 
 /* Super Ultra Pro RameshDADA Premium 1Dot */
 
 app.initializers.add('ramesh-dada-mobile-flarum-newdiscuss', () => {
-  extend(IndexPage.prototype, 'viewItems', function (items) {
+  extend(IndexPage.prototype, 'sidebarItems', (items) => {
     const canStartDiscussion = app.forum.attribute('canStartDiscussion') || !app.session.user;
     items.add('newDiscussion',
       Button.component(
@@ -23,6 +24,15 @@ app.initializers.add('ramesh-dada-mobile-flarum-newdiscuss', () => {
         },
         app.translator.trans(canStartDiscussion ? 'core.forum.index.start_discussion_button' : 'core.forum.index.cannot_start_discussion_button')
       ), -100);
+
+    // Add new item
+    items.add(
+      'searchbutton',
+      <Button icon="fas fa-search" className="Button Button--primary SearchDada" onclick={() => app.modal.show(searchmodal, {})}>
+        Search
+      </Button>,
+      89
+    );
 
     const tag = this.currentTag();
     if (tag) {
